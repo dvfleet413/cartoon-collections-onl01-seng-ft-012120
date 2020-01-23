40bd405291c55b63a08ed5cabf7e0afb7f60597d
David Van Fleet <dvfleet413@gmail.com>
2020-01-23 04:53:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,10 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|element| element.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.collect do |ingredient|
+    return ingredient if cheese_types.include?(ingredient)
+  end
 end
